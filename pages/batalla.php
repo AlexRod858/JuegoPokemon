20c1f74e3e922f9453e13add6968a6e7be2a1091
AlexRod <oa.defrag@gmail.com>
2023-11-12 01:29:13 +0000

    logica de batalla al 70%

--- pages/batalla.php
+++ pages/batalla.php
@@ -1,14 +1,11 @@
 <?php include('../partials/header.php') ?>
 <?php include('../Pokemon.php') ?>
 
-
-<!-- -------------------------- -->
+<!-- Contenido superior -->
 <div style="background-image: url(https://images5.alphacoders.com/608/608323.png); background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover; width: auto; min-height: 98vh;">
-    <!-- Contenido superior -->
-    <!-- -------------------------- -->
-    <!-- -------------------------- -->
+    background-repeat: no-repeat;
+    background-size: cover; width: auto; min-height: 90vh;">
+
     <a href="../index.php" class="btn btn-primary ms-2 mt-2">Inicio</a>
     <div class="row text-center pt-3">
         <div class="col-4">
@@ -17,7 +14,9 @@
             </span>
         </div>
         <div class="col-4">
-            <span class="fs-1">0:05:00</span>
+            <!-- Contenido -->
+            <div id="temporizador" class="fs-1"></div>
+            <button id="pelea" class="btn btn-success">¡PELEAR!</button>
         </div>
         <div class="col-4">
             <span class="fs-1">
@@ -25,69 +24,53 @@
             </span>
         </div>
     </div>
-    <!-- -------------------------- -->
-    <!-- -------------------------- -->
+
     <?php $equipo_rival = equipo_rival() ?>
 
-    <!-- -------------------------- -->
-    <!-- -------------------------- -->
     <div class="row">
-    <div class="col-4 d-flex flex-column align-items-center justify-content-end" style="min-height: 90vh;" id="usuario-container">
-        <!-- Contenido del usuario -->
-        <img src="" alt="Imagen del usuario" id="usuario-imagen" style="width: 500px; height: auto;">
+        <div class="col-4 d-flex flex-column align-items-center justify-content-end" style="min-height: 90vh;" id="usuario-container">
+            <!-- Contenido del usuario -->
+            <img src="" alt="Imagen del usuario" id="usuario-imagen" style="width: 500px; height: auto;">
+        </div>
+        <div class="col-4">
+            <!-- Contenido -->
+        </div>
+        <div class="col-4 d-flex flex-column align-items-center justify-content-end" style="min-height: 80vh;" id="rival-container">
+            <!-- Contenido del rival -->
+            <img src="" alt="Imagen del rival" id="rival-imagen" style="width: 500px; height: auto;">
+        </div>
     </div>
-    <div class="col-4">
-        <!-- Contenido -->
-    </div>
-    <div class="col-4 d-flex flex-column align-items-center justify-content-end" id="rival-container">
-        <!-- Contenido del rival -->
-        <img src="" alt="Imagen del rival" id="rival-imagen" style="width: 500px; height: auto;">
-    </div>
-</div>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        var cinturonUsuario = <?php echo json_encode($cinturon); ?>;
-        var cinturonRival = <?php echo json_encode($equipo_rival); ?>;
-        var index = 0;
+    <!-- Script JavaScript -->
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            var cinturonUsuario = <?php echo json_encode($cinturon); ?>;
+            var cinturonRival = <?php echo json_encode($equipo_rival); ?>;
+            var index = 0; // Cambiado a 1 para que inicie con la segunda pareja
 
-        function mostrarSiguientePokemon() {
-            document.getElementById('usuario-imagen').src = cinturonUsuario[index].imagen;
-            document.getElementById('rival-imagen').src = cinturonRival[index].imagen;
-            index++;
+            // Función para mostrar la siguiente pareja
+            function mostrarSiguientePareja() {
+                document.getElementById('usuario-imagen').src = cinturonUsuario[index].imagen;
+                document.getElementById('rival-imagen').src = cinturonRival[index].imagen;
+                index++;
 
-            // Reiniciar el índice si se han mostrado todos los Pokémon
-            if (index >= cinturonUsuario.length) {
-                index = 0;
+                // Reiniciar el índice si se han mostrado todas las parejas
+                if (index >= cinturonUsuario.length) {
+                    index = 0;
+                }
             }
 
-            // Esperar 5 segundos antes de mostrar el siguiente Pokémon
-            setTimeout(mostrarSiguientePokemon, 5000);
-        }
+            // Mostrar la primera pareja al cargar la página
+            mostrarSiguientePareja();
 
-        // Iniciar el proceso al cargar la página
-        mostrarSiguientePokemon();
-    });
-</script>
-
-
-    <!-- ------------------------- -->
-    <!-- ------------------------- -->
-
+            // Agregar evento click al botón "PELEAR"
+            document.getElementById('pelea').addEventListener('click', function () {
+                mostrarSiguientePareja();
+                // iniciarTemporizador(); 
+            });
+        });
+    </script>
 </div>
-
-
-<!-- ------------------------- -->
-<!-- ------------------------- -->
-
-</div>
-
-
-
-
-
-
-
 
 
 
